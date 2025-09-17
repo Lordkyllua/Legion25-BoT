41af0527ffdee2a3164fbe65d25bf3f0c18c31b7
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-17 18:49:41 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,8 @@
-...
+module.exports = {
+  name: "ping",
+  description: "Verifica latencia del bot",
+  async execute(message) {
+    const sent = await message.channel.send("🏓 Pong!");
+    sent.edit(`🏓 Pong! Latencia: **${sent.createdTimestamp - message.createdTimestamp}ms**`);
+  }
+};
