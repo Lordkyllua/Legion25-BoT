0a1b79ae9b13091284195a36271ae49c83d46fa1
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-17 18:46:24 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,8 @@
-...
+module.exports = {
+  name: "ready",
+  once: true,
+  execute(client) {
+    console.log(`✅ Bot conectado como ${client.user.tag}`);
+    client.user.setActivity("🎮 Juegos y roles", { type: 0 });
+  }
+};
