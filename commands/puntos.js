8443e8c10e488a1e01987fb94d45270e4cc1cc45
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-17 22:10:56 -0300

    Delete commands/puntos.js

--- commands/puntos.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "puntos",
-  description: "Muestra puntos de usuario",
-  async execute(message, args, client) {
-    const userId = message.author.id;
-    const points = client.points.get(userId) || 0;
-    message.reply(`⭐ ${message.author.username}, tienes **${points} puntos**.`);
-  }
-};
