5bf18e1a5314c521081ca76809fd38ea76ad5a51
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-17 21:56:09 -0300

    Delete events/messageCreate.js

--- events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "messageCreate",
-  async execute(message, client) {
-    if (message.author.bot) return;
-    const userId = message.author.id;
-    const current = client.points.get(userId) || 0;
-    client.points.set(userId, current + 1);
-  }
-};
