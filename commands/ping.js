682103d91116b399de436f3a9fc9a68de791c8b0
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-17 22:11:26 -0300

    Delete commands/ping.js

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Verifica latencia del bot",
-  async execute(message) {
-    const sent = await message.channel.send("🏓 Pong!");
-    sent.edit(`🏓 Pong! Latencia: **${sent.createdTimestamp - message.createdTimestamp}ms**`);
-  }
-};
