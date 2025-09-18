2013559886b017f7316553b52fb93d12985b462a
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-18 07:34:45 -0300

    Crear state.js

---
+++ utils/state.js
@@ -0,0 +1,18 @@
+function rotateStatus(client) {
+  const statuses = [
+    "Tiny Survivors ⚔️",
+    "Collecting points ⭐",
+    "Use !help 📜",
+    "Developed by LordK 💻"
+  ];
+  let i = 0;
+  setInterval(() => {
+    client.user.setPresence({
+      activities: [{ name: statuses[i], type: 0 }],
+      status: "online"
+    });
+    i = (i + 1) % statuses.length;
+  }, 60000); // cada minuto
+}
+
+module.exports = { rotateStatus };
