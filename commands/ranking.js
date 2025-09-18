ed031947234285cbd024ccde1ab0b10e67c48bab
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-18 06:48:30 -0300

    Crear ranking.js

---
+++ commands/ranking.js
@@ -0,0 +1,11 @@
+const db = require("../utils/database");
+
+module.exports = {
+  name: "ranking",
+  description: "Show top users",
+  execute(message) {
+    const top = db.getRanking();
+    const leaderboard = top.map((u, i) => `${i + 1}. <@${u.id}> - ⭐ ${u.points}`).join("\n");
+    message.channel.send(`🏆 **Ranking**\n${leaderboard}`);
+  }
+};
