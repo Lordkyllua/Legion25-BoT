633e3b10fbb80a550ae1cb9e496e1956ab5ed647
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-18 07:49:22 -0300

    Actualizar ranking.js

--- commands/ranking.js
+++ commands/ranking.js
@@ -5,6 +5,7 @@ module.exports = {
   description: "Show top users",
   execute(message) {
     const top = db.getRanking();
+    if (!top.length) return message.channel.send("🏆 No points yet!");
     const leaderboard = top.map((u, i) => `${i + 1}. <@${u.id}> - ⭐ ${u.points}`).join("\n");
     message.channel.send(`🏆 **Ranking**\n${leaderboard}`);
   }
