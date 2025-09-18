b219a4d49e86ce9a76c2529ab83bcc2ce30bb2eb
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-18 06:49:04 -0300

    Crear dice.js

---
+++ commands/dice.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "dado",
+  description: "Roll a dice",
+  execute(message) {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    message.reply(`🎲 You rolled a **${roll}**!`);
+  }
+};
