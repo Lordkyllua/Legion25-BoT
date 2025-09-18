2e8b867f893ed4c1ceff66fb635ee969df4b8dac
Lordkyllua <117127321+Lordkyllua@users.noreply.github.com>
2025-09-18 07:52:47 -0300

    Actualizar messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,10 +6,10 @@ module.exports = {
   execute(message, client) {
     if (message.author.bot) return;
 
-    // Sistema de puntos
+    // Add points for activity
     db.addPoints(message.author.id, 1);
 
-    // Prefijo
+    // Commands
     if (!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
